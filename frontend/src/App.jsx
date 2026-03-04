e126c8727759bb515364bad81c0f147529bcc779
Mena <dayjacs03@gmail.com>
2026-03-03 20:27:00 -0600

    working on index components + signup + login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,14 @@ import { OutletContext } from "react-dom";
 
 function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function checkIfUser() {
+      //api for jwt;
+    }
+    checkIfUser();
+  }, []);
+
   return <div></div>;
 }
 
