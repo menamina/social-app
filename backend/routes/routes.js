35bfb3a3c1343e6766d3f63ef309f5f8da8dce14
Mena <dayjacs03@gmail.com>
2026-03-04 18:18:20 -0600

    setting up schema

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,72 +3,68 @@ const router = express.Router();
 const { isAuth } = require("../utils/passportAuth");
 const remote = require("../controller/remote");
 const passport = require("../passport/passport");
-const validators = require("../utils/validator");
+const validator = require("../utils/validator");
 
 router.get("/api/isAuth", isAuth, (req, res, next) => {
-    res.json({
-        user: req.user 
-    })
-})
+  res.json({
+    user: req.user,
+  });
+});
 
 router.post("/login", (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-        if  (err) return next(err);
-        if (!user) {
-            return res.status(401).json({ message: info.message})
-        }
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.status(401).json({ message: info.message });
+    }
 
-        req.login(user, (err) => {
-             if (err) return next(err);
+    req.login(user, (err) => {
+      if (err) return next(err);
 
-            return res.json({
-                user: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
+      return res.json({
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
         },
       });
-        })
-    })
+    });
+  });
 });
 
+router.post("/sign-up", isAuth, validator, remote.signup);
 
-router.post("/sign-up", (req, res, next) => {
-    req.logout((error) => {
-        if (error){
-            return next(error)
-        }
-        res.clearCookie("connect.sid")
-        res.status(200).json({ success: true })
-    })
+router.post("/logout", (req, res, next) => {
+  req.logout((error) => {
+    if (error) {
+      return next(error);
+    }
+    res.clearCookie("connect.sid");
+    res.status(200).json({ success: true });
+  });
 });
-
-router.post("/logout", (req, res){
-
-})
 
 router.get("/", isAuth, remote.home);
 router.get("/profile", isAuth, remote.profile);
 router.get("/search", isAuth, remote.home);
 router.get("/settings", isAuth, remote.settings);
 
+// posts //
+router.post("/post", isAuth, remote.post);
+router.delete("/deletePost", isAuth, remote.deletePost);
 
-// posts // 
-router.post("/post", isAuth, remote.post)
-router.delete("/deletePost", isAuth, remote.deletePost)
+router.post("/repost", isAuth, remote.repost);
+router.delete("/removeRepost", isAuth, remote.removeRepost);
 
-router.post("/repost", isAuth, remote.repost)
-router.delete("/removeRepost", isAuth, remote.removeRepost)
-
-router.post("/like", isAuth, remote.like)
-router.delete("/removeLike", isAuth, remote.removeLike)
+router.post("/like", isAuth, remote.like);
+router.delete("/removeLike", isAuth, remote.removeLike);
 
 // comments //
-router.post("/comment", isAuth, remote.comment)
-router.post("/deleteComment", isAuth, remote.deleteComment)
+router.post("/comment", isAuth, remote.comment);
+router.post("/deleteComment", isAuth, remote.deleteComment);
 
 // search //
-router.get("/search", isAuth, remote.search)
+router.get("/search", isAuth, remote.search);
 
 // update profile //
-router.patch("/update-profile", isAuth, remote.updateProfile)
+router.patch("/update-profile", isAuth, remote.updateProfile);
