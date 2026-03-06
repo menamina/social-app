6c813790191b218717dabc07315fc5f7a8c532af
Mena <dayjacs03@gmail.com>
2026-03-05 18:24:18 -0600

    adding password bcrypt

--- backend/utils/validator.js
+++ backend/utils/validator.js
@@ -1,25 +1,38 @@
 const { body, validationResult } = require("express-validator");
 
 const validator = [
-    body("name").notEmpty().withMessage("Your name is required")
-    ,
-    
-    body("username").trim().notEmpty().withMessage("A username is required"),
-    
-    body("email").trim().notEmpty().withMessage("An email is required").isEmail().withMessage("A valid email address is required"),
-    
-    body("password").notEmpty()
+  body("name").notEmpty().withMessage("Your name is required"),
+
+  body("username").trim().notEmpty().withMessage("A username is required"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("An email is required")
+    .isEmail()
+    .withMessage("A valid email address is required"),
+
+  body("password")
+    .notEmpty()
     .withMessage("Password is required")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters"),
-    
-    body("confirmpassord".custom(value, { req }) => {
-        if (value !== req.body.password){
-            throw new Error("Passwords must match")
-        } else {
-            return true
-        }
-    })
-]
 
-module.exports = validator
+  body("confirmpassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords must match");
+    }
+    return true;
+  }),
+
+  (req, res, next) => {
+    const errors = validationResult(validator);
+
+    if (errors.isEmpty()) {
+      return next();
+    }
+    return res.status(400).json({ validationErrs: errors.array() });
+  },
+];
+
+module.exports = validator;
