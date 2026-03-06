19e0f36675f38823d7924c11f6efd7a9ea14fefb
Mena <dayjacs03@gmail.com>
2026-03-05 18:02:21 -0600

    validators

--- backend/utils/validator.js
+++ backend/utils/validator.js
@@ -3,11 +3,22 @@ const { body, validationResult } = require("express-validator");
 const validator = [
     body("name").notEmpty().withMessage("Your name is required")
     ,
+    
     body("username").trim().notEmpty().withMessage("A username is required"),
+    
     body("email").trim().notEmpty().withMessage("An email is required").isEmail().withMessage("A valid email address is required"),
-    body("password"),
+    
+    body("password").notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
+    
     body("confirmpassord".custom(value, { req }) => {
-
+        if (value !== req.body.password){
+            throw new Error("Passwords must match")
+        } else {
+            return true
+        }
     })
 ]
 
