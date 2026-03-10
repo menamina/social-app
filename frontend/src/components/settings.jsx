cc1eec0c5d23106e94fa47427d4eb82430b8b9f3
Mena <dayjacs03@gmail.com>
2026-03-09 20:33:18 -0500

    settings.jsx

--- frontend/src/components/settings.jsx
+++ frontend/src/components/settings.jsx
@@ -2,47 +2,36 @@ import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 
 function Settings() {
-    const {userProfile, setUserProfile} = useOutletContext();
-    const [viewOpt, setViewOpt] = useState("privacy")
+  const { userProfile, setUserProfile } = useOutletContext();
+  const [viewOpt, setViewOpt] = useState("privacy");
 
-    function changeViewOpt(option){
+  function changeViewOpt(option) {
+    setViewOpt(option);
+  }
 
-        switch (option) {
-            case "privacy":
-                setViewOpt("privacy");
-            case "account":
-                setViewOpt("account");
-            case "help":
-                 setViewOpt("account");
-        }
-    }
-
-    return (
-        <div className="settingsDiv">
-            <div>
-                <div>
-                    <p>back</p>
-                    {/* change to back error */}
-                </div>
-                <div>
-                    Settings
-                </div>
-            </div>
-            <div className="settingsFlexContainer">
-                <div>
-                    <div onClick={() => changeViewOpt("privacy")}>Privacy</div>
-                    <div onClick={() => changeViewOpt("account")}>Account</div>
-                    <div onClick={() => changeViewOpt("help")}>Help</div>
-                </div>
-                <div>
-                    {viewOpt === "privacy" ? <div>/<div> : null}
-                </div>
-
-            </div>
-
+  return (
+    <div className="settingsDiv">
+      <div>
+        <div>
+          <p>back</p>
+          {/* change to back error */}
         </div>
-    )
-
+        <div>Settings</div>
+      </div>
+      <div className="settingsFlexContainer">
+        <div>
+          <div onClick={() => changeViewOpt("privacy")}>Privacy</div>
+          <div onClick={() => changeViewOpt("account")}>Account</div>
+          <div onClick={() => changeViewOpt("help")}>Help</div>
+        </div>
+        <div>
+          {viewOpt === "privacy" ? <div></div> : null}
+          {viewOpt === "account" ? <div></div> : null}
+          {viewOpt === "help" ? <div></div> : null}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Settings;
