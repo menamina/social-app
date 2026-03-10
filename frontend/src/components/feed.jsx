7d475d0f286518a60bda5c1946b5e5b2e33a5d98
Mena <dayjacs03@gmail.com>
2026-03-09 19:19:44 -0500

    feed.jsx

--- frontend/src/components/feed.jsx
+++ frontend/src/components/feed.jsx
@@ -7,9 +7,39 @@ function Feed() {
   const { forYouFeed, setForYouFeed, forYouFeedErr, setForYouFeedErr } =
     OutletContext();
 
-  async function refreshForYouFeed() {}
+  const [followingFeed, setFollowingFeed] = useState(null);
 
-  async function refreshForFollowingFeed() {}
+  const [feedView, setFeedView] = useState("for you");
+
+  async function forYouRefresh() {}
+
+  async function followingRefresh() {}
+
+  return (
+    <div className="feed">
+      <div className="feedOpts">
+        <div onClick={forYouRefresh}>For You</div>
+        <div>|</div>
+        <div onClick={followingRefresh}>Following</div>
+      </div>
+
+      <div className="feedPosts">
+        <div>
+          <div>
+            <img />
+          </div>
+          <div>
+            <input placeholder="Wanna say something?" />
+          </div>
+        </div>
+        {feedView === "for you" ? (
+          <div className="forYouPosts"></div>
+        ) : (
+          <div className="followingPosts"></div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
