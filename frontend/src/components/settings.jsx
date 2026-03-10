7f58b7a4a566b7b04752661c05f4eeb4dfd86c3e
Mena <dayjacs03@gmail.com>
2026-03-09 20:31:37 -0500

    settings.jsx

--- frontend/src/components/settings.jsx
+++ frontend/src/components/settings.jsx
@@ -1,6 +1,48 @@
 import { useState, useEffect } from "react";
-import { OutletContext } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 
-function Settings() {}
+function Settings() {
+    const {userProfile, setUserProfile} = useOutletContext();
+    const [viewOpt, setViewOpt] = useState("privacy")
+
+    function changeViewOpt(option){
+
+        switch (option) {
+            case "privacy":
+                setViewOpt("privacy");
+            case "account":
+                setViewOpt("account");
+            case "help":
+                 setViewOpt("account");
+        }
+    }
+
+    return (
+        <div className="settingsDiv">
+            <div>
+                <div>
+                    <p>back</p>
+                    {/* change to back error */}
+                </div>
+                <div>
+                    Settings
+                </div>
+            </div>
+            <div className="settingsFlexContainer">
+                <div>
+                    <div onClick={() => changeViewOpt("privacy")}>Privacy</div>
+                    <div onClick={() => changeViewOpt("account")}>Account</div>
+                    <div onClick={() => changeViewOpt("help")}>Help</div>
+                </div>
+                <div>
+                    {viewOpt === "privacy" ? <div>/<div> : null}
+                </div>
+
+            </div>
+
+        </div>
+    )
+
+}
 
 export default Settings;
