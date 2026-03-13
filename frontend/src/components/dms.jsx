ee603668763c5924f8c813134b999facc04d9b7e
Mena <dayjacs03@gmail.com>
2026-03-13 18:11:31 -0500

    main.jsx update

--- frontend/src/components/dms.jsx
+++ frontend/src/components/dms.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+// add a search for user functionality IN the dms
+
 function Dms() {
   useEffect(() => {}, []);
 }
