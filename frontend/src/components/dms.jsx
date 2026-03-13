6d008fcb7b04cf0d4370e5f1cc15b4949b3788ed
Mena <dayjacs03@gmail.com>
2026-03-13 18:13:22 -0500

    dms.jsx

--- frontend/src/components/dms.jsx
+++ frontend/src/components/dms.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // add a search for user functionality IN the dms
+// when adding message from here check blocked
 
 function Dms() {
   useEffect(() => {}, []);
