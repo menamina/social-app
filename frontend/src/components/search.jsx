58223746f4219432380afa8ac700663d30901e08
Mena <dayjacs03@gmail.com>
2026-03-12 20:16:27 -0500

    working on search.jsx

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -4,14 +4,43 @@ import { useOutletContext } from "react-router-dom";
 function Search() {
   const [query, setQuery] = useState("");
   const [queryResults, setQueryResults] = useState(null);
+  const [noQueryToReturn, setNoQueryToReturn] = useState(null)
   const [queryError, setQueryError] = useState(null);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    async function searchRes() {
-      setInterval(() => {}, 3000);
-    }
-    searchRes();
-  }, [query]);
+    setLoading(true)
+    const timeout = setTimeout(() => {
+        async function queryRes(){
+            const res = await fetch("http://localhost:5555/search", {
+                method: "GET",
+                credentials: "include",
+                body: JSON.stringify({
+                    query
+                })
+            })
+
+            const data = await res.json()
+
+            if (!res.ok){
+                setNoQueryToReturn(data.noResult)
+                setLoading(false)
+                return
+            }
+
+            setQueryResults(data.queryResults)
+            setLoading(false)
+            return
+        },
+        queryRes()
+    }, 3000),
+
+    return () => clearTimeout(timeout);
+  }, [query])
+  
+
+
+
 }
 
 export default Search;
