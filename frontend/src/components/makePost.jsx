ee8afd37b21886cd86325c160b5653976b4252e2
Mena <dayjacs03@gmail.com>
2026-03-17 19:01:11 -0500

    working on making post

--- frontend/src/components/makePost.jsx
+++ frontend/src/components/makePost.jsx
@@ -1,3 +1,20 @@
-function MakeAPost() {}
+import { useState, useEffect } from "react";
+import { Link, useOutletContext } from "react-router-dom";
+
+function MakeAPost() {
+  return (
+    <div className="makeAPost div">
+      <div>
+        <button>cancel</button>
+        <div>new post</div>
+        <img src="" alt="" />
+      </div>
+      <div></div>
+      <div>
+        <button>post</button>
+      </div>
+    </div>
+  );
+}
 
 export default MakeAPost;
